7835669a2ab31263aa908f9735f4d6b9f06f326f
ValterPereiraSilva <valterpereira280667@gmail.com>
2020-01-02 15:16:41 -0300

    Implementing simple search

--- SalesWebMvc1/Models/SalesRecord.cs
+++ SalesWebMvc1/Models/SalesRecord.cs
@@ -5,10 +5,12 @@ namespace SalesWebMvc1.Models
 {
     public class SalesRecord
     {
+  
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
 
         public SalesRecord()
         {
